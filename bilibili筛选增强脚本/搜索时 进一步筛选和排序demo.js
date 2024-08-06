88fc7a439ac09ff4bfed14aaebdc7c90c99072b5
Intellijc <1242849765@qq.com>
2024-08-06 14:06:33 +0800

    806: 解决发布日期错误问题,实现导入params进行评分后筛选,写回页面使用异步函数,预筛选函数添加参数
    难点:return sortedVideos.map(item => item.video); 排序函数计算出score,返回的是{score,video} 返回如果是 sortedVideos.video是错的

--- bilibili筛选增强脚本/搜索时 进一步筛选和排序demo.js
+++ bilibili筛选增强脚本/搜索时 进一步筛选和排序demo.js
@@ -6,12 +6,12 @@
 // @icon         https://www.bilibili.com/favicon.ico?v=1
 // ==/UserScript==
 
-; (function () {
+;(function () {
     'use strict'
 
     //---------------------------------------------------------预筛选------------------------------------------------
 
-        function filterAndExcludeByDate(cardList, excludeUids, minDanmaku, minPlayCount, dateStart, dateEnd) {
+    function filterAndExcludeByDate(cardList, excludeUids, minDanmaku, minPlayCount, dateStart, dateEnd) {
         let preInfoAndCard = [];
         console.log('初步筛选开始');
 
@@ -19,19 +19,31 @@
         cardList.forEach(card => {
 
             // 如果URL包含 '/cheese/play/'说明是bili课堂，跳过该卡片
-            if (card.querySelector('a').href.includes('/cheese/play/')) { console.log('跳过课堂卡片:', card.querySelector('.bili-video-card__info--tit').textContent.trim()); return;}
+            if (card.querySelector('a').href.includes('/cheese/play/')) {
+                console.log('跳过课堂卡片:', card.querySelector('.bili-video-card__info--tit').textContent.trim());
+                return;
+            }
             // 如果URL包含'/live.bilibili.com'说明是bili直播,跳过该卡片
-            if (card.querySelector('a').href.includes('/live.bilibili.com/')) {console.log('跳过直播卡片:', card.querySelector('.bili-video-card__info--tit').textContent.trim());return;}
+            if (card.querySelector('a').href.includes('/live.bilibili.com/')) {
+                console.log('跳过直播卡片:', card.querySelector('.bili-video-card__info--tit').textContent.trim());
+                return;
+            }
             // 如果 card没有bili-video-card__info--owner属性, 说明是搜up得来的视频,跳过该卡片 todo:获取up信息并收录该卡片
-            if (!card.querySelector('.bili-video-card__info--owner')){console.log('跳过up主介绍卡片:', card.querySelector('.bili-video-card__info--tit').textContent.trim());return;}
+            if (!card.querySelector('.bili-video-card__info--owner')) {
+                console.log('跳过up主介绍卡片:', card.querySelector('.bili-video-card__info--tit').textContent.trim());
+                return;
+            }
 
             let preUpUid = card.querySelector('.bili-video-card__info--owner').getAttribute('href').split('/').pop();
             let preBvId = card.querySelector('.bili-video-card__info--right a[href^="//www.bilibili.com/video/BV"]').getAttribute('href').match(/\/BV[\w]+\//)[0].replace(/\//g, '');
             let preDanmaku = parseInt(card.querySelector('.bili-video-card__stats--item:nth-child(2) span').textContent);
             let prePlayCountText = card.querySelector('.bili-video-card__stats--item:nth-child(1) span').textContent.trim();
             let prePlayCount;//如果包含万 ,去万乘10000
-            if (prePlayCountText.includes('万')) { prePlayCount = parseFloat(prePlayCountText.replace('万', '')) * 10000; }
-            else { prePlayCount = parseInt(prePlayCountText); }
+            if (prePlayCountText.includes('万')) {
+                prePlayCount = parseFloat(prePlayCountText.replace('万', '')) * 10000;
+            } else {
+                prePlayCount = parseInt(prePlayCountText);
+            }
 
             let preDuration = card.querySelector('.bili-video-card__stats__duration').textContent;
             let prePublishDate = card.querySelector('.bili-video-card__info--date').textContent.replace('·', '').trim();
@@ -47,31 +59,40 @@
                 preVideoName,
                 card
             });
-            console.log('获取到card:',preUpUid,preBvId,preDanmaku,prePlayCount,preDuration, prePublishDate,preVideoName);
+            console.log('获取到card:', preUpUid, preBvId, preDanmaku, prePlayCount, preDuration, prePublishDate, preVideoName);
         });
         console.log('获取到preInfoAndCard:', preInfoAndCard);
 
-        // 筛选视频
-        let preFilteredInfoAndCardList = preInfoAndCard.filter(video => {
+        // 返回预筛选后的BV号列表
+        return preInfoAndCard.filter(video => {
             // 排除指定UP主
-            if (excludeUids.includes(video.preUpUid)) { console.log(`up主在黑名单,排除《${video.preVideoName}》`); return false; }
+            if (excludeUids.includes(video.preUpUid)) {
+                console.log(`up主在黑名单,排除《${video.preVideoName}》`);
+                return false;
+            }
 
             // 检查弹幕数
-            if (video.preDanmaku < minDanmaku ) { console.log(`弹幕数量=${video.preDanmaku},排除《${video.preVideoName}》`); return false; }
+            if (video.preDanmaku < minDanmaku) {
+                console.log(`弹幕数量=${video.preDanmaku},排除《${video.preVideoName}》`);
+                return false;
+            }
 
             // 检查播放量
-            if (video.prePlayCount < minPlayCount) { console.log(`播放量=${video.prePlayCount},排除《${video.preVideoName}》`); return false; }
+            if (video.prePlayCount < minPlayCount) {
+                console.log(`播放量=${video.prePlayCount},排除《${video.preVideoName}》`);
+                return false;
+            }
 
             // 检查发布日期
             let start = new Date(dateStart);
             let end = new Date(dateEnd);
-            if (Date(video.prePublishDate) < start || Date(video.prePublishDate) > end) { console.log(`发布日期不在范围内,排除《${video.preVideoName}》`); return false; }
+            if (Date(video.prePublishDate) < start || Date(video.prePublishDate) > end) {
+                console.log(`发布日期不在范围内,排除《${video.preVideoName}》`);
+                return false;
+            }
 
             return true;
         });
-
-        // 返回筛选后的BV号列表
-        return preFilteredInfoAndCardList;
     }
 
     //---------------------------------------------------------进一步筛选------------------------------------------------
@@ -83,7 +104,7 @@
         let videoDetailsArray = [];
         for (const preFilteredInfoAndCard of preFilteredInfoAndCardList) {
             let bvId = preFilteredInfoAndCard.card.querySelector('.bili-video-card__info--right a[href^="//www.bilibili.com/video/BV"]').getAttribute('href').match(/\/BV[\w]+\//)[0].replace(/\//g, '');
-            console.log('测试bv号:',bvId)
+            console.log('测试bv号:', bvId)
             const response = await fetch(`https://api.bilibili.com/x/web-interface/view?bvid=${bvId}`);
             const data = await response.json();
             if (data.code === 0) {
@@ -92,7 +113,7 @@
                     //todo:增加预筛选获取字段
                     bvid: videoData.bvid, // 视频的BV号
                     title: videoData.title, // 视频标题
-                    pubdate: new Date(videoData.pubdate * 1000), // 发布日期，转换为日期对象
+                    pubdate: preFilteredInfoAndCard.prePublishDate, // 发布日期
                     duration: videoData.duration, // 视频时长，单位为秒
                     view: videoData.stat.view.toString().includes('万') ? parseFloat(videoData.stat.view.toString().replace('万', '')) * 10000 : parseInt(videoData.stat.view.toString()), // 处理后的播放量
                     danmaku: videoData.stat.danmaku, // 弹幕数量
@@ -126,9 +147,18 @@
             const favoriteViewRatioActual = video.favorite / video.view;
             const coinViewRatioActual = video.coin / video.view;
 
-            if (likeViewRatioActual < likeViewRatio) {console.log(`点赞播放比= ${likeViewRatioActual}, 排除视频《${video.title}》`);return false;}
-            if (favoriteViewRatioActual < favoriteViewRatio) { console.log(`收藏播放比= ${favoriteViewRatioActual}, 排除视频《${video.title}》`); return false; }
-            if (coinViewRatioActual < coinViewRatio) { console.log(`投币播放比= ${coinViewRatioActual}, 排除视频《${video.title}》`); return false; }
+            if (likeViewRatioActual < likeViewRatio) {
+                console.log(`点赞播放比= ${likeViewRatioActual}, 排除视频《${video.title}》`);
+                return false;
+            }
+            if (favoriteViewRatioActual < favoriteViewRatio) {
+                console.log(`收藏播放比= ${favoriteViewRatioActual}, 排除视频《${video.title}》`);
+                return false;
+            }
+            if (coinViewRatioActual < coinViewRatio) {
+                console.log(`投币播放比= ${coinViewRatioActual}, 排除视频《${video.title}》`);
+                return false;
+            }
 
             return true;
         });
@@ -136,45 +166,64 @@
 
     // 函数：排序视频
     // 参数：videoDetailsArray - 视频详细信息数组
+    // 参数：params - 计算得分的参数
     // 返回值：排序后的视频数组
-    function sortVideos(videoDetailsArray, sortBy) {
+    function sortVideos(videoDetailsArray, params) {
         console.log('开始排序视频');
-        return videoDetailsArray.sort((a, b) => b[sortBy] - a[sortBy]);
+        // 计算得分
+        const sortedVideos = videoDetailsArray.map(video => {
+            const view = video.view + params.k1;
+            const score =
+                params.a * (video.like / Math.pow(view, params.b)) +
+                params.c * (video.favorite / Math.pow(view, params.b)) +
+                params.d * (video.coin / Math.pow(view, params.b)) +
+                params.e * (video.reply / (video.view + params.k2)) +
+                params.f * video.duration +
+                params.g * video.share;
+            return {video, score};
+        });
+
+        sortedVideos.sort((a, b) => b.score - a.score);// 排序
+
+        // 输出排序结果
+        console.log('最终筛选和排序后的视频列表:');
+        sortedVideos.forEach(item => {
+            console.log(`得分: ${item.score}, 视频名称:${item.video.title}`);
+        });
+
+        return sortedVideos.map(item => item.video);
     }
 
-    // 函数：视频按序写回页面 fatal:解决筛选出数量和显示的不一致  todo:识别并去掉纪录片/番剧/up主/活动
+
+    // 函数：视频按序写回页面 fatal:解决筛选出数量和显示的不一致  todo:识别并去掉纪录片/番剧/up主/活动的标题画面
     // 参数：sortedArray - 排序后的视频信息数组
     function reorderVideos(sortedArray) {
+
         const container = document.evaluate('//*[@id="i_cecream"]/div/div[2]/div[2]/div/div/div/div[2]/div', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
         if (!container) {
             console.log('找不到视频列表容器');
             return;
         }
 
-
-
         // 删除除了sortedArray.length个视频卡片之外的所有视频卡片及其父元素
         while (container.children.length > sortedArray.length) {
             container.removeChild(container.lastChild);
         }
-
-        // 重新排序视频元素
+        const videoContainers = container.children;
+        // 重新写回视频元素
         for (let i = 0; i < sortedArray.length; i++) {
-            // 删除原有的card
-            container.children[i].innerHTML = '';
-            // 添加新的card
-            container.children[i].appendChild(sortedArray[i].card);
+            container.children[i].innerHTML = '';// 删除原有的card
+            videoContainers[i].appendChild(sortedArray[i].card);// 添加新的card
+            console.log(`写入第${i + 1}个视频卡片:${sortedArray[i].title}`);
         }
-
-
     }
 
 
-    // 主函数：执行筛选和排序
+    // 主函数：执行筛选,排序并写回页面
     async function main() {
 
         //预筛选视频卡片
-        let preFilteredCardList = filterAndExcludeByDate()//todo 待添加参数
+        let preFilteredCardList = filterAndExcludeByDate(cardList, excludeUids, minDanmaku, minPlayCount, dateStart, dateEnd)
 
         // 获取所有视频的详细信息
         let videoDetailsArray = await getVideoDetailsArray(preFilteredCardList);
@@ -184,19 +233,14 @@
         console.log('进一步筛选后的视频:', furtherFilteredVideos);
 
         // 排序
-        let sortedVideos = sortVideos(furtherFilteredVideos, 'coin');
-        console.log('排序后的视频:', sortedVideos);
-
-        // 输出结果到控制台
-        console.log('最终筛选和排序后的视频列表:', sortedVideos.map(video => video.bvid));
+        let sortedVideos = sortVideos(furtherFilteredVideos, params);
 
         // 视频按序写回页面
-        reorderVideos(sortedVideos);
+        await reorderVideos(sortedVideos);
     }
 
 
     //-------------------------------------------------------------
-
 
 
     // 模拟预筛选后的结果，使用当前页面的前三个卡片
@@ -204,16 +248,18 @@
     let minDanmaku = 7;
     let minPlayCount = 2000;
     let dateStart = '2020-01-01';
-    let dateEnd = '2024-07-31';
-    let likeViewRatio = 0.05; // 赞播比
+    let dateEnd = '2024-08-06';
+    let likeViewRatio = 0.01 // 赞播比
     let favoriteViewRatio = 0.008; // 藏播比
     let coinViewRatio = 0.0045; // 币播比
+    let cardList;//视频卡片列表
+    const params = {a: 0.4, b: 1.5, c: 0.3, d: 0.2, e: 0.1, f: 0.05, g: 0.1, k1: 10, k2: 100};
 
     // 等待页面加载完成
-    window.addEventListener('load', function() {
+    window.addEventListener('load', function () {
         console.log('页面加载完成, 等待1秒后执行脚本');
         setTimeout(async () => {
-            let cardList = Array.from(document.querySelectorAll('.bili-video-card')).slice(0, 20);//todo 待添加翻页填满功能(不着急)
+            cardList = Array.from(document.querySelectorAll('.bili-video-card')).slice(0, 10);//todo 待添加翻页填满功能(不着急)
             console.log('模拟数据获取成功:', cardList);
             // 执行主函数
             await main();
